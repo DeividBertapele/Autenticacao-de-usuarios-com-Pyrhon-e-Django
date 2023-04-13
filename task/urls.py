391bafaa81cccce51113fa3824d75c91caa2bae0
DeividBertapele <deividbertapele@gmail.com>
2023-04-13 10:42:09 -0300

    Incluindo os urls

--- task/urls.py
+++ task/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name='home'),
+    path('sigup/', views.sigup, name='sigup'),
+    path('sigin/', views.sigin, name='sigin'),
+    path('sair/', views.sair, name='sair'),
+    path('tasks/', views.tasks, name='tasks'),
+
 ]
